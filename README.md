a6a0469757b8bb69504fbb498dbeed83618874a9
huangzhike <heavenlystate@163.com>
2018-09-12 20:39:47 +0800

    set up

---
+++ README.md
@@ -0,0 +1,28 @@
+# SIM = SHIT IM
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+
+
+http://google.github.io/material-design-icons/#icon-font-for-the-web
+
+https://wangdahoo.github.io/vonic-documents/#/
+https://material.io/tools/icons/?style=baseline
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+
+
+        <HelloWorld msg="Welcome to SIM"/>
+
+
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from "@/components/HelloWorld.vue";
+
+
+    export default {
+        name: "home",
+
+        data() {
+            return {};
+        },
+        methods: {},
+        components: {
+            HelloWorld
+
+        }
+    };
+</script>
